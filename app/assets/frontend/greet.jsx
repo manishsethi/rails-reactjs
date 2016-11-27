9dcc5bb3b1387f39bfd2971c72892566ea6a8f8c
Manish Sethi <manish@sethis.in>
2016-11-27 09:23:45 +0000

    added webpack

---
+++ app/assets/frontend/greet.jsx
@@ -0,0 +1,8 @@
+
+export default class Greet extends React.Component {
+  render(){
+    return (
+      <h1>Hello from Watch me daily</h1>
+    );
+  }
+}
---
+++ app/assets/javascripts/bundle.js
@@ -0,0 +1,144 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// shortcut for better minimizing
+/******/ 	var exports = "exports";
+/******/ 	
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function require(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId][exports];
+/******/ 		
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/ 		
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module[exports], module, module[exports], require);
+/******/ 		
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/ 		
+/******/ 		// Return the exports of the module
+/******/ 		return module[exports];
+/******/ 	}
+/******/ 	
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	require.modules = modules;
+/******/ 	
+/******/ 	// expose the module cache
+/******/ 	require.cache = installedModules;
+/******/ 	
+/******/ 	// __webpack_public_path__
+/******/ 	require.p = "";
+/******/ 	
+/******/ 	
+/******/ 	// Load entry module and return exports
+/******/ 	return require(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/*!**************************************!*\
+  !*** ./app/assets/frontend/main.jsx ***!
+  \**************************************/
+/***/ function(module, exports, require) {
+
+	'use strict';
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	var _greet = require(/*! ./greet */ 1);
+	
+	var _greet2 = _interopRequireDefault(_greet);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Main = function (_React$Component) {
+	  _inherits(Main, _React$Component);
+	
+	  function Main() {
+	    _classCallCheck(this, Main);
+	
+	    return _possibleConstructorReturn(this, (Main.__proto__ || Object.getPrototypeOf(Main)).apply(this, arguments));
+	  }
+	
+	  _createClass(Main, [{
+	    key: 'render',
+	    value: function render() {
+	      return React.createElement(_greet2.default, null);
+	    }
+	  }]);
+	
+	  return Main;
+	}(React.Component);
+	
+	var documentReady = function documentReady() {
+	  React.render(React.createElement(Main, null), document.getElementById('react'));
+	};
+	
+	$(documentReady);
+
+/***/ },
+/* 1 */
+/*!***************************************!*\
+  !*** ./app/assets/frontend/greet.jsx ***!
+  \***************************************/
+/***/ function(module, exports, require) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var Greet = function (_React$Component) {
+	  _inherits(Greet, _React$Component);
+	
+	  function Greet() {
+	    _classCallCheck(this, Greet);
+	
+	    return _possibleConstructorReturn(this, (Greet.__proto__ || Object.getPrototypeOf(Greet)).apply(this, arguments));
+	  }
+	
+	  _createClass(Greet, [{
+	    key: "render",
+	    value: function render() {
+	      return React.createElement(
+	        "h1",
+	        null,
+	        "Hello from Watch me daily"
+	      );
+	    }
+	  }]);
+	
+	  return Greet;
+	}(React.Component);
+	
+	exports.default = Greet;
+
+/***/ }
+/******/ ])
+/*
+//@ sourceMappingURL=bundle.js.map
+*/
