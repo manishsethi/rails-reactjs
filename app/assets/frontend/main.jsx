93789a7d4a38ca1504ebb2fbece3f4f9bd4564e5
root <manish@sethis.in>
2016-12-06 07:46:43 +0000

    First run for store

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,11 +1,11 @@
-import TweetBox from "./components/TweetBox"
-import TweetList from "./components/TweetList"
+import TweetBox from "./components/TweetBox";
+import TweetList from "./components/TweetList";
+import TweetStore from "./stores/TweetStore";
 
-let mockTweets = [
-  {id: 1, name: 'Manish Sethi', body: 'My #FirstTweet'},
-  {id: 2, name: 'Manish Sethi', body: 'My #FirstTweet'},
-  {id: 3, name: 'Manish Sethi', body: 'My #FirstTweet'}
-];
+import TweetActions from "./actions/TweetActions";
+
+TweetActions.getAllTweets();
+
 
 
 class Main extends React.Component {
@@ -13,28 +13,28 @@ class Main extends React.Component {
     super(props);
     this.state = {tweetsList: []};
   }
-  formattedTweets(tweetsList){
-    let formattedList = tweetsList.map(tweet => {
-      tweet.formattedDate = moment(tweet.created_at).fromNow();
-      return tweet;
-    });
-    return {
-      tweetsList: formattedList
-    }
-  }
+  // formattedTweets(tweetsList){
+  //   let formattedList = tweetsList.map(tweet => {
+  //     tweet.formattedDate = moment(tweet.created_at).fromNow();
+  //     return tweet;
+  //   });
+  //   return {
+  //     tweetsList: formattedList
+  //   }
+  // }
   addTweet(tweetToAdd){
-    $.post("/tweets", {body: tweetToAdd})
-    .success( savedTweet => {
-      let newTweetsList = this.state.tweetsList;
-      newTweetsList.unshift(savedTweet);
-      this.setState(this.formattedTweets(newTweetsList));
-  })
-  .error(error => console.log(error));
+  //   $.post("/tweets", {body: tweetToAdd})
+  //   .success( savedTweet => {
+  //     let newTweetsList = this.state.tweetsList;
+  //     newTweetsList.unshift(savedTweet);
+  //     this.setState(this.formattedTweets(newTweetsList));
+  // })
+  // .error(error => console.log(error));
 }
   componentDidMount(){
-    $.ajax("/tweets")
-    .success(data => this.setState(this.formattedTweets(data)))
-    .error(error => console.log(error));
+    // $.ajax("/tweets")
+    // .success(data => this.setState(this.formattedTweets(data)))
+    // .error(error => console.log(error));
   }
   render(){
     return (
